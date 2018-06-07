6fa511dde1a57456dd6f7676890ff7de98eb27df
mirekphd <36706320+mirekphd@users.noreply.github.com>
2018-06-07 14:18:31 +0200

    updated repo name

--- rebuild_repo.sh
+++ rebuild_repo.sh
@@ -1,12 +1,12 @@
 mkdir -p ~/git/devel
 cd ~/git/devel
-sudo rm -r ml-cpu-r35-guac
+sudo rm -r docker-vnc
 # set git password cache to timeout after 1 hour (3600 seconds)
 git config --global credential.helper 'cache --timeout=3600'
-git clone --recursive --branch devel https://github.com/mirekphd/ml-cpu-r35-guac.git
-cd ml-cpu-r35-guac
+git clone --recursive --branch devel https://github.com/mirekphd/docker-vnc.git
+cd docker-vnc
 sudo chmod +x rebuild_repo.sh
-docker build --tag mirekphd/ml-cpu-r35-guac .
+docker build --tag mirekphd/docker-vnc .
 
 
 
